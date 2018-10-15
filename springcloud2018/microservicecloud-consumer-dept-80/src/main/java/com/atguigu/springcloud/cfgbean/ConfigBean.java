96297683150f2950d6ea27def1b71519f9bff7e4
niepu1991 <niepu1991@163.com>
2018-10-16 00:18:32 +0800

    consumer的api构建

--- springcloud2018/microservicecloud-consumer-dept-80/src/main/java/com/atguigu/springcloud/cfgbean/ConfigBean.java
+++ springcloud2018/microservicecloud-consumer-dept-80/src/main/java/com/atguigu/springcloud/cfgbean/ConfigBean.java
@@ -13,6 +13,8 @@ import org.springframework.web.client.RestTemplate;
 @Configuration
 public class ConfigBean {
     // boot applicationContext.xml ---@Configuration配置  configBean=applicationContext.xml
+    // RestTemplate提供很多便捷访问远程HTTP服务的方法
+    // 是一种简洁的访问restful服务模板类，spring提供的访问rest服务的客户端
     @Bean
     public RestTemplate getRestTemplate(){
         return new RestTemplate();
---
+++ springcloud2018/microservicecloud-consumer-dept-80/src/main/java/com/atguigu/springcloud/controller/DeptControllerConsumer.java
@@ -0,0 +1,47 @@
+package com.atguigu.springcloud.controller;
+
+import com.atguigu.springcloud.entities.Dept;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.List;
+
+/**
+ * @description:
+ * @author:niepu
+ * @version:1.0
+ * @date:2018/10/15 23:50
+ **/
+@RestController
+public class DeptControllerConsumer {
+    // 消费者，所以不需要关注service层；直接使用controller层通过
+    // restTemplate
+    /*
+    * 使用restTemplate访问restful接口非常简单
+    * (url,requestMap,ResponseBean.class)
+    * */
+
+    private static final String REST_URL_PREFIX="http://localhost:8001";
+
+    @Autowired
+    private RestTemplate restTemplate;
+
+    @RequestMapping(value = "/consumer/dept/add")
+    public boolean add(Dept dept){
+        return restTemplate.postForObject(REST_URL_PREFIX+"/dept/add",dept,Boolean.class);
+    }
+
+    @RequestMapping(value = "/consumer/dept/get/{id}")
+    public Dept get(@PathVariable("id") Long id){
+        return restTemplate.getForObject(REST_URL_PREFIX+"/dept/get/"+id,Dept.class);
+    }
+
+    @RequestMapping(value = "/consumer/dept/list")
+    public List<Dept> list(@PathVariable("id") Long id){
+        return restTemplate.getForObject(REST_URL_PREFIX+"/dept/list",List.class);
+    }
+
+}
