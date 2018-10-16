02e623bcae85edf567c384badcb0d2aaee32e26f
niepu1991 <niepu1991@163.com>
2018-10-16 21:46:19 +0800

    Dept服务提供者改造之给启动类添加@EnableEurekaClient注解

--- springcloud2018/microservicecloud-provider-dept-8001/src/main/java/com/atguigu/springcloud/DeptProvider8001_App.java
+++ springcloud2018/microservicecloud-provider-dept-8001/src/main/java/com/atguigu/springcloud/DeptProvider8001_App.java
@@ -2,6 +2,7 @@ package com.atguigu.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * @description:
@@ -10,6 +11,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @date:2018/10/15 22:46
  **/
 @SpringBootApplication
+@EnableEurekaClient
 public class DeptProvider8001_App {//类名加上端口号，方便区分
     public static void main(String[] args) {
         SpringApplication.run(DeptProvider8001_App.class,args);
