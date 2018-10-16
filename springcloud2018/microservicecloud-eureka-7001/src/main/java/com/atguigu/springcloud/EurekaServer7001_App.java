e7a86e1c1e25bb36646aeacdaaeb5248b6796e63
niepu1991 <niepu1991@163.com>
2018-10-16 21:25:48 +0800

    添加启动类：@SpringBootAppication和@EnableEurekaServer

--- springcloud2018/microservicecloud-eureka-7001/src/main/java/com/atguigu/springcloud/EurekaServer7001_App.java
+++ springcloud2018/microservicecloud-eureka-7001/src/main/java/com/atguigu/springcloud/EurekaServer7001_App.java
@@ -1,10 +1,19 @@
 package com.atguigu.springcloud;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
 /**
  * @description:
  * @author:niepu
  * @version:1.0
  * @date:2018/10/16 21:16
  **/
+@SpringBootApplication
+@EnableEurekaServer // 不能忘记
 public class EurekaServer7001_App {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServer7001_App.class,args);
+    }
 }
