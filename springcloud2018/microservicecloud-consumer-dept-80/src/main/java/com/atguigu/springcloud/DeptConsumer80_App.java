558c342c85ab78f10daaa5586c73fe607de041f5
niepu1991 <niepu1991@163.com>
2018-10-18 22:25:04 +0800

    添加80消费者服务到eureka注册中心

--- springcloud2018/microservicecloud-consumer-dept-80/src/main/java/com/atguigu/springcloud/DeptConsumer80_App.java
+++ springcloud2018/microservicecloud-consumer-dept-80/src/main/java/com/atguigu/springcloud/DeptConsumer80_App.java
@@ -2,6 +2,8 @@ package com.atguigu.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * @description:
@@ -10,6 +12,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @date:2018/10/16 0:19
  **/
 @SpringBootApplication
+@EnableEurekaClient
+@EnableDiscoveryClient
 public class DeptConsumer80_App {
 
     public static void main(String[] args) {
