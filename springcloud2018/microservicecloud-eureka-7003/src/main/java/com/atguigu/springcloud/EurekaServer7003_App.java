63086a90d2683cad24178d048f89f6c7b5add863
niepu1991 <niepu1991@163.com>
2018-10-19 20:59:48 +0800

    eureka集群之7002和7003配置文件和启动类的处理

---
+++ springcloud2018/microservicecloud-eureka-7003/src/main/java/com/atguigu/springcloud/EurekaServer7003_App.java
@@ -0,0 +1,19 @@
+package com.atguigu.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @description:
+ * @author:niepu
+ * @version:1.0
+ * @date:2018/10/19 20:17
+ **/
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaServer7003_App {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServer7003_App.class,args);
+    }
+}
