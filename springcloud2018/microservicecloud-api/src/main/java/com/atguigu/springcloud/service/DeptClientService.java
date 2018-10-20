1ed29a1c50310d8367993e39759cb8fd71bf0b26
niepu1991 <niepu1991@163.com>
2018-10-20 17:44:36 +0800

    feign-添加microservicecloud-api的调用接口

---
+++ springcloud2018/microservicecloud-api/src/main/java/com/atguigu/springcloud/service/DeptClientService.java
@@ -0,0 +1,26 @@
+package com.atguigu.springcloud.service;
+
+import com.atguigu.springcloud.entities.Dept;
+import org.springframework.cloud.netflix.feign.FeignClient;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import java.util.List;
+
+/**
+ * @description:
+ * @author:niepu
+ * @version:1.0
+ * @date:2018/10/20 17:37
+ **/
+@FeignClient(value = "MICROSERVICECLOUD-DEPT")
+public interface DeptClientService {
+    @RequestMapping(value = "/dept/add",method = RequestMethod.POST)
+    boolean add(@RequestBody Dept dept);
+    @RequestMapping(value = "/dept/get/{id}",method = RequestMethod.GET)
+    Dept get(@PathVariable("id") Long id);
+    @RequestMapping(value = "/dept/list",method = RequestMethod.GET)
+    List<Dept> list();
+}
