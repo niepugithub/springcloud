48132c27259e54fb33e528781011a6b51a0ae67c
niepu1991 <niepu1991@163.com>
2018-10-20 21:02:52 +0800

    hytrix-fallback统一处理-microservicecloud-api修改

---
+++ springcloud2018/microservicecloud-api/src/main/java/com/atguigu/springcloud/service/DeptClientServiceFallbackFactory.java
@@ -0,0 +1,43 @@
+package com.atguigu.springcloud.service;
+
+import com.atguigu.springcloud.entities.Dept;
+import feign.hystrix.FallbackFactory;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+/**
+ * @description:根据已有的DeptClientService
+ * 接口新建一个实现了FallbackFactory接口的类
+ * DeptClientServiceFallbackFactory
+ * 实现统一的短路保护；之前是一个接口方法一个fallback短路保护方法
+ * 麻烦。借用spring aop思想
+ * 将DeptClientService中所有方法的fallback方法集中处理
+ * @author:niepu
+ * @version:1.0
+ * @date:2018/10/20 20:54
+ **/
+@Component  // 不要忘记
+public class DeptClientServiceFallbackFactory implements FallbackFactory<DeptClientService> {
+
+    @Override
+    public DeptClientService create(Throwable throwable) {
+
+        return new DeptClientService() {
+            @Override
+            public boolean add(Dept dept) {
+                return false;
+            }
+
+            @Override
+            public Dept get(Long id) {
+                return new Dept(id,"该id没有对应部门","no source");
+            }
+
+            @Override
+            public List<Dept> list() {
+                return null;
+            }
+        };
+    }
+}
