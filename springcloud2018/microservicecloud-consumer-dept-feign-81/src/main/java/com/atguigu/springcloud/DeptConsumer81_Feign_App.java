b647918587e36bd6304a914b8c3311c33227f06e
niepu1991 <niepu1991@163.com>
2018-10-20 17:54:29 +0800

    feign-修改81消费者主启动类注解

--- springcloud2018/microservicecloud-consumer-dept-feign-81/src/main/java/com/atguigu/springcloud/DeptConsumer81_Feign_App.java
+++ springcloud2018/microservicecloud-consumer-dept-feign-81/src/main/java/com/atguigu/springcloud/DeptConsumer81_Feign_App.java
@@ -3,6 +3,8 @@ package com.atguigu.springcloud;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.feign.EnableFeignClients;
+import org.springframework.context.annotation.ComponentScan;
 
 /**
  * @description:
@@ -12,9 +14,11 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
  **/
 @SpringBootApplication
 @EnableEurekaClient
-public class DeptConsumer80_Feign_App {
+@EnableFeignClients(basePackages = "com.atguigu.springcloud")
+@ComponentScan("com.atguigu.springcloud")
+public class DeptConsumer81_Feign_App {
 
     public static void main(String[] args) {
-        SpringApplication.run(DeptConsumer80_Feign_App.class,args);
+        SpringApplication.run(DeptConsumer81_Feign_App.class,args);
     }
 }
--- springcloud2018/microservicecloud-consumer-dept-feign-81/src/main/java/com/atguigu/springcloud/controller/DeptControllerConsumer.java
+++ springcloud2018/microservicecloud-consumer-dept-feign-81/src/main/java/com/atguigu/springcloud/controller/DeptControllerConsumer.java
@@ -6,7 +6,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
 
 import java.util.List;
 
