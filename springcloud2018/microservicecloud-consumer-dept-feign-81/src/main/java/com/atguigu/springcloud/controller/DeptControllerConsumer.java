934897d413a8ac7cbdda5a17a4815784f1f73392
niepu1991 <niepu1991@163.com>
2018-10-20 17:49:06 +0800

    feign-81消费者使用deptClientService替代restTemplate

--- springcloud2018/microservicecloud-consumer-dept-feign-81/src/main/java/com/atguigu/springcloud/controller/DeptControllerConsumer.java
+++ springcloud2018/microservicecloud-consumer-dept-feign-81/src/main/java/com/atguigu/springcloud/controller/DeptControllerConsumer.java
@@ -1,6 +1,7 @@
 package com.atguigu.springcloud.controller;
 
 import com.atguigu.springcloud.entities.Dept;
+import com.atguigu.springcloud.service.DeptClientService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,35 +18,22 @@ import java.util.List;
  **/
 @RestController
 public class DeptControllerConsumer {
-    // 消费者，所以不需要关注service层；直接使用controller层通过
-    // restTemplate
-    /*
-    * 使用restTemplate访问restful接口非常简单
-    * (url,requestMap,ResponseBean.class)
-    * */
 
-    // private static final String REST_URL_PREFIX="http://localhost:8001";
-    private static final String REST_URL_PREFIX="http://MICROSERVICECLOUD-DEPT";
     @Autowired
-    private RestTemplate restTemplate;
+    private DeptClientService deptClientService;
 
     @RequestMapping(value = "/consumer/dept/add")
     public boolean add(Dept dept){
-        return restTemplate.postForObject(REST_URL_PREFIX+"/dept/add",dept,Boolean.class);
+        return deptClientService.add(dept);
     }
 
     @RequestMapping(value = "/consumer/dept/get/{id}")
     public Dept get(@PathVariable("id") Long id){
-        return restTemplate.getForObject(REST_URL_PREFIX+"/dept/get/"+id,Dept.class);
+        return deptClientService.get(id);
     }
 
     @RequestMapping(value = "/consumer/dept/list")
     public List<Dept> list(){
-        return restTemplate.getForObject(REST_URL_PREFIX+"/dept/list",List.class);
-    }
-
-    @RequestMapping("/consumer/dept/discovery")
-    public Object discovery(){
-        return restTemplate.getForObject(REST_URL_PREFIX+"/dept/discovery",Object.class);
+        return deptClientService.list();
     }
 }
