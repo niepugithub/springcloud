38e4de69339d5c6124415c8d882bd162498c0fe7
niepu1991 <niepu1991@163.com>
2018-10-20 17:35:02 +0800

    feign-拷贝基于ribbon的80到81，添加pom依赖，改端口，改主类名

---
+++ springcloud2018/microservicecloud-consumer-dept-feign-81/src/main/java/com/atguigu/springcloud/DeptConsumer80_Feign_App.java
@@ -0,0 +1,20 @@
+package com.atguigu.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * @description:
+ * @author:niepu
+ * @version:1.0
+ * @date:2018/10/16 0:19
+ **/
+@SpringBootApplication
+@EnableEurekaClient
+public class DeptConsumer80_Feign_App {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DeptConsumer80_Feign_App.class,args);
+    }
+}
---
+++ springcloud2018/microservicecloud-consumer-dept-feign-81/src/main/java/com/atguigu/springcloud/cfgbean/ConfigBean.java
@@ -0,0 +1,33 @@
+package com.atguigu.springcloud.cfgbean;
+
+import com.netflix.loadbalancer.IRule;
+import com.netflix.loadbalancer.RandomRule;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+
+/**
+ * @description:
+ * @author:niepu
+ * @version:1.0
+ * @date:2018/10/15 23:43
+ **/
+@Configuration
+public class ConfigBean {
+    // boot applicationContext.xml ---@Configuration配置  configBean=applicationContext.xml
+    // RestTemplate提供很多便捷访问远程HTTP服务的方法
+    // 是一种简洁的访问restful服务模板类，spring提供的访问rest服务的客户端
+    @Bean
+    @LoadBalanced // springcloud基于netflix ribbon实现一套基于客户端的负载均衡工具
+    public RestTemplate getRestTemplate(){
+        return new RestTemplate();
+    }
+
+    // 添加其他负载均衡算法；默认是轮询，RoundRobinRule
+    // 使用自定义的AbstractLoadBalancerRule，下面的这个注释掉
+//    @Bean
+//    public IRule myRule(){
+//        return new RandomRule();
+//    }
+}
