c4481518ed28add7171ff1bc50a666cd6a3ef87f
niepu1991 <niepu1991@163.com>
2018-10-20 20:19:25 +0800

    hystrix-8004拷贝8001，修改启动类名和yml接口，instanceId

---
+++ springcloud2018/microservicecloud-provider-dept-hystrix-8004/src/main/java/com/atguigu/springcloud/DeptProvider8004_Hystrix_App.java
@@ -0,0 +1,21 @@
+package com.atguigu.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * @description:
+ * @author:niepu
+ * @version:1.0
+ * @date:2018/10/15 22:46
+ **/
+@SpringBootApplication
+@EnableEurekaClient
+@EnableDiscoveryClient // 添加此注解，方便从eureka注册中心找到其他客户端
+public class DeptProvider8004_Hystrix_App {//类名加上端口号，方便区分
+    public static void main(String[] args) {
+        SpringApplication.run(DeptProvider8004_Hystrix_App.class,args);
+    }
+}
