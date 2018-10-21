1f911dcc370d8e44eb882f92d51e3d2ff4311f83
niepu1991 <niepu1991@163.com>
2018-10-21 17:55:36 +0800

    springcloud-config-服务端3344换远程github仓库地址

---
+++ springcloud2018/microservicecloud-config-client-3355/src/main/java/com/atguigu/springcloud/ConfigClient_3355_StartSpringCloudApp.java
@@ -0,0 +1,10 @@
+package com.atguigu.springcloud;
+
+/**
+ * @description:
+ * @author:niepu
+ * @version:1.0
+ * @date:2018/10/21 17:35
+ **/
+public class ConfigClient_3355_StartSpringCloudApp {
+}
---
+++ springcloud2018/microservicecloud-config-client-3355/src/main/java/com/atguigu/springcloud/rest/ConfigClientRest.java
@@ -0,0 +1,10 @@
+package com.atguigu.springcloud.rest;
+
+/**
+ * @description:
+ * @author:niepu
+ * @version:1.0
+ * @date:2018/10/21 17:27
+ **/
+public class ConfigClientRest {
+}
