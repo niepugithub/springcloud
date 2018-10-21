2bc66de87afb09160eb2d6109e3bd82c989f759a
niepu1991 <niepu1991@163.com>
2018-10-21 17:56:55 +0800

    springcloud-config-客户端3355模块成功通过config-server3344从github获取到配置信息

--- springcloud2018/microservicecloud-config-client-3355/src/main/java/com/atguigu/springcloud/ConfigClient_3355_StartSpringCloudApp.java
+++ springcloud2018/microservicecloud-config-client-3355/src/main/java/com/atguigu/springcloud/ConfigClient_3355_StartSpringCloudApp.java
@@ -1,10 +1,17 @@
 package com.atguigu.springcloud;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 /**
  * @description:
  * @author:niepu
  * @version:1.0
  * @date:2018/10/21 17:35
  **/
+@SpringBootApplication
 public class ConfigClient_3355_StartSpringCloudApp {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigClient_3355_StartSpringCloudApp.class,args);
+    }
 }
--- springcloud2018/microservicecloud-config-client-3355/src/main/java/com/atguigu/springcloud/rest/ConfigClientRest.java
+++ springcloud2018/microservicecloud-config-client-3355/src/main/java/com/atguigu/springcloud/rest/ConfigClientRest.java
@@ -1,10 +1,28 @@
 package com.atguigu.springcloud.rest;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 /**
  * @description:
  * @author:niepu
  * @version:1.0
  * @date:2018/10/21 17:27
  **/
+@RestController
 public class ConfigClientRest {
+    @Value("${spring.application.name}")
+    private String applicationName;
+    @Value("${eureka.client.service-url.defaultZone}")
+    private String eurekaServers;
+    @Value("${server.port}")
+    private String port;
+
+    @RequestMapping("/config")
+    public String getConfig(){
+        String str="applicationName:"+applicationName+", eurekaServers:"+eurekaServers+", port:"+port;
+        System.out.println("str==="+str);
+        return str;
+    }
 }
